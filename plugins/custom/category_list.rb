ee4976ca8a6f80220b3c28b6b720f1b79b41b0c7
David Tchepak <dave@davesquared.net>
2012-01-14 03:16:07 +1100

    Adding category list index page
    Added category_link filter to Octopress' category_generator.r
    Added custom CategoryListPage and CategoryListGenerator plugin.

---
+++ plugins/custom/category_list.rb
@@ -0,0 +1,43 @@
+module Jekyll
+  class CategoryListPage < Page
+    def initialize(site, base, dir, layout, categories)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), layout + '.html')
+      self.data['category_list'] = categories
+    end
+  end
+
+  class CategoryListGenerator < Generator
+    safe true
+
+    def generate(site)
+      return nil unless site.layouts.key? 'category_list'
+      category_list = site.categories.keys.sort.collect { |c| CategoryItem.new(c, site.categories[c].size) }
+
+      dir = site.config['category_dir']
+
+      page = CategoryListPage.new(site, site.source, dir, 'category_list', category_list)
+      page.render(site.layouts, site.site_payload)
+      page.write(site.dest)
+      site.static_files << page
+    end
+
+    class CategoryItem
+      def initialize(name, post_count)
+        @name = name
+        @post_count = post_count
+      end
+      def to_s
+          @name
+      end
+      def to_liquid
+        { "name" => @name, "post_count" => @post_count }
+      end
+    end
+  end
+end
